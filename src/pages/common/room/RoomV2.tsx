6ac137eb847bdffe2d1b1f32bd66ce90c2e256d5
KyAnh1126 <anh261104@gmail.com>
2025-05-23 11:47:04 +0700

    update video-call feature, add more buttons

--- src/pages/common/room/RoomV2.tsx
+++ src/pages/common/room/RoomV2.tsx
@@ -3,21 +3,16 @@ import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom"
 import VideoPlayer from "./components/VideoPlayer";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
+import { Mic, MicOff, Video, VideoOff, X } from "lucide-react";
 
 export default function Room() {
 
     const navigate = useNavigate();
     const { id : roomId } = useParams();
-    const { ws, myPeer, stream, peers, myCall } = useContext(RoomContext);
+    const { ws, myPeer, stream, peers, myCall, handleToogleAudio, handleToogleVideo,handleClickEndMeeting } = useContext(RoomContext);
 
-    function handleClickEndMeeting() {
-        if(myCall) {
-            myCall.close();
-            ws.emit('leave-room');
-        }
-        navigate('/');
-    }
+    let audioEnabled = stream.getAudioTracks()[0].enabled;
+    let videoEnabled = stream.getVideoTracks()[0].enabled;
 
     return (
         <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center p-4">
@@ -27,9 +22,20 @@ export default function Room() {
                         <VideoPlayer key={peer.id} className="w-full h-full" stream={peer.stream} isMuted={false}/>
                     )
                 })}
-                <Button className="flex flex-row rounded-lg gap-2" onClick={handleClickEndMeeting}>
-                    <X/> <div>End meeting</div>
-                </Button>
+                <div className="flex flex-row gap-4">
+
+                    <Button className="flex flex-row gap-2 rounded-lg items-center" onClick={handleToogleAudio}>
+                        {audioEnabled ? <Mic/> : <MicOff/>}
+                    </Button>
+
+                    <Button className="flex flex-row gap-2 rounded-lg items-center" onClick={handleToogleVideo}>
+                        {videoEnabled ? <Video/> : <VideoOff/>}
+                    </Button>
+
+                    <Button className="flex flex-row gap-2 rounded-lg items-center" onClick={handleClickEndMeeting}>
+                        <X/> <div>End meeting</div>
+                    </Button>
+                </div>
             </div>
             <div className="fixed bottom-8 right-4 w-[20vw] h-[20vh]">
                 <VideoPlayer className="w-full h-full" stream={stream} isMuted={true}/>
